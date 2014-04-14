62188562ec0c19bfc8f3fc281d037e1bc0ddb9ac
Logan Bell <lbell@shutterstock.com>
2014-04-14 18:50:29 -0400

    Added UserAgent::Curl to source

---
+++ lib/MooseX/Role/REST/Consumer/UserAgent/Curl.pm
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+
+package MooseX::Role::REST::Consumer::UserAgent::Curl;
+
+use Moose;
+extends 'WWW::Curl::UserAgent';
+
+sub agent {
+  shift->user_agent_string(@_);
+}
+
+#FIXME: version of libcurl that we have has a bug in subsecond timeouts
+has '+connect_timeout' => ( default => sub { 1000 } );
+
+sub default_headers { };
+sub use_eval {};
+
+sub timeout {
+  my ($self) = shift;
+  if(@_) {
+    my $timeout = $_[0] * 1000;
+    $self->SUPER::timeout($timeout);
+  } else {
+    return $self->SUPER::timeout;
+  }
+};
+
+__PACKAGE__->meta->make_immutable;
+1;
